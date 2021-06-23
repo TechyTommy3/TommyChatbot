aea161afe0ce6b5ed246b47c9005d00534cd01d3
Tommy Steensen <thesteensens@gmail.com>
2021-06-23 07:46:52 -0500

    Add no-leak notice.

--- TommyChatbot/Source.cpp
+++ TommyChatbot/Source.cpp
@@ -10,7 +10,7 @@ int Chatbot() {
 	cout << "You:";
 	getline(cin, message);
 	if (message == "Who are you?") {
-		cout << "This is TommyChatbot. This is not copyrighted.";
+		cout << "This is TommyChatbot. This is not copyrighted. Please do not leak this dev";
 	}
 	else if (message == "I have a virus.") {
 		cout << "I would reccomend doing a full scan of your antivirus of choice.";
